5f5fc453b594107dc9840497c74d638d92119f12
Lidor Repo <lidormalich@gmail.com>
2018-02-02 13:56:45 +0200

    ARM Update

--- הורדות/Gapps package/מעבד/ARM/4.4/install_GAPPS.bat
+++ הורדות/Gapps package/מעבד/ARM/4.4/install_GAPPS.bat
@@ -5,7 +5,7 @@ adb kill-server
 adb start-server
 @echo Preparing to install all Gapps files to device
 @pause
-@for /f "delims=|" %%f in ('dir /b "D:\GAPPS\ARM\4.4\"*.apk') do @"D:\GAPPS\ARM\4.4\ADB\adb.exe" install -r "D:\GAPPS\ARM\4.4\%%f"
+@for /f "delims=|" %%f in ('dir /b "C:\GAPPS\ARM\4.4\"*.apk') do @"C:\GAPPS\ARM\4.4\ADB\adb.exe" install -r "C:\GAPPS\ARM\4.4\%%f"
 @echo End of Batch File
 @echo Done! Reboot You're Phone!
 adb reboot 
